0a3cafcb2ab3099901e08ed89614db583ce840a8
Peter Meerwald-Stadler <pmeerw@pmeerw.net>
2021-02-10 23:43:38 +0100

    Add ELF support

---
+++ VTIL-Common/formats/elf.cpp
@@ -0,0 +1,182 @@
+// Copyright (c) 2021 Peter Meerwald-Stadler and contributors of the VTIL Project   
+// All rights reserved.   
+//    
+// Redistribution and use in source and binary forms, with or without   
+// modification, are permitted provided that the following conditions are met: 
+//    
+// 1. Redistributions of source code must retain the above copyright notice,   
+//    this list of conditions and the following disclaimer.   
+// 2. Redistributions in binary form must reproduce the above copyright   
+//    notice, this list of conditions and the following disclaimer in the   
+//    documentation and/or other materials provided with the distribution.   
+// 3. Neither the name of VTIL Project nor the names of its contributors
+//    may be used to endorse or promote products derived from this software 
+//    without specific prior written permission.   
+//    
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE   
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR   
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF   
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS   
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN   
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)   
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE.        
+//
+#include "elf.hpp"
+#include "../io/asserts.hpp"
+#include <string.h>
+#include "../math/bitwise.hpp"
+
+namespace vtil
+{
+#pragma pack(push, 4)
+	// Magic constants
+	//
+	static constexpr uint32_t ELF_MAGIC = 0x464c457f; // "\x7fELF"
+
+	// Class / Format
+	//
+	enum class format_id : uint8_t
+	{
+		elf32 = 0x01,
+		elf64 = 0x02,
+	};
+
+	// Endiness
+	//
+	enum class endiness_id : uint8_t
+	{
+		little = 0x01,
+		big = 0x02,
+	};
+
+	// OS ABI
+	//
+	enum class osabi_id : uint8_t
+	{
+		systemv = 0x00,
+		hpux = 0x01,
+		netbsd = 0x02,
+		linnux = 0x03,
+	};
+
+	// Object File Type
+	//
+	enum class type_id : uint16_t
+	{
+		none = 0x0000,
+		rel = 0x0001,
+		exec = 0x0002,
+		dyn = 0x0003,
+	};
+
+	// Machine
+	//
+	enum class machine_id : uint16_t
+	{
+		none = 0x0000,
+		amd64 = 0x003e,
+	};
+
+	// File header
+	//
+	struct file_header_t
+	{
+		uint32_t magic;
+		format_id format;
+		endiness_id endiness;
+		uint8_t version;
+		osabi_id os_abi;
+		uint8_t abi_version;
+		uint8_t _pad[7];
+		type_id type;
+		machine_id machine;
+	};
+
+	// Program header
+	//
+	struct program_header_t
+	{
+		//type;
+		//flags;
+	};
+
+#pragma pack(pop)
+
+	bool elf_image::is_elf64() const
+	{
+        if ( !is_valid() )
+            return false;
+
+		return true;
+	}
+
+	uint64_t elf_image::get_image_base() const
+	{
+		// Get the image base from optional header.
+		//
+
+		return 0;
+	}
+
+	size_t elf_image::get_image_size() const
+	{
+		// Get the image size from optional header.
+		//
+
+		return 0;
+	}
+
+	bool elf_image::has_relocations() const
+	{
+		// Relocs must not be stripped and basereloc should exist.
+		//
+
+		return false;
+	}
+
+	std::optional<uint64_t> elf_image::get_entry_point() const
+	{
+		// Get the entry point from optional header, return nullopt if zero.
+		//
+
+		return std::nullopt;
+	}
+
+	bool elf_image::is_valid() const
+	{
+		// Get image boundaries and the file header.
+		//
+		const void* data = cdata();
+		const void* data_limit = ( char* ) cdata() + size();
+		auto file_header = ( const file_header_t* ) cdata();
+		
+		// Validate DOS header.
+		//
+		if ( size() < sizeof(file_header->magic) || file_header->magic != ELF_MAGIC ) 
+			return false;
+
+		// TODO: Validate more data...
+		//
+		return true;
+	}
+	
+	void elf_image::flush()
+	{
+	}
+
+	void elf_image::add_section( section_descriptor& in_out, const void* data, size_t size )
+	{
+	}
+
+	void elf_image::enum_relocations( const function_view<bool( const relocation_descriptor& )>& fn ) const
+	{
+	}
+
+	void elf_image::delete_relocation( const relocation_descriptor& desc )
+	{
+	}
+};
---
+++ VTIL-Common/formats/elf.hpp
@@ -0,0 +1,75 @@
+// Copyright (c) 2021 Peter Meerwald-Stadler and contributors of the VTIL Project   
+// All rights reserved.   
+//    
+// Redistribution and use in source and binary forms, with or without   
+// modification, are permitted provided that the following conditions are met: 
+//    
+// 1. Redistributions of source code must retain the above copyright notice,   
+//    this list of conditions and the following disclaimer.   
+// 2. Redistributions in binary form must reproduce the above copyright   
+//    notice, this list of conditions and the following disclaimer in the   
+//    documentation and/or other materials provided with the distribution.   
+// 3. Neither the name of VTIL Project nor the names of its contributors
+//    may be used to endorse or promote products derived from this software 
+//    without specific prior written permission.   
+//    
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE   
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR   
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF   
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS   
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN   
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)   
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE.        
+//
+#pragma once
+#include <vector>
+#include "image_descriptor.hpp"
+
+namespace vtil
+{
+	// Describes a 64/32 bit ELF Image.
+	//
+	struct elf_image : image_descriptor
+	{
+		// Construct by raw byte array.
+		//
+		std::vector<uint8_t> raw_bytes;
+		elf_image( const std::vector<uint8_t>& raw_bytes = {} ) : raw_bytes( raw_bytes ) {}
+		
+		// Default move/copy.
+		//
+		elf_image( elf_image&& ) = default;
+		elf_image( const elf_image& ) = default;
+		elf_image& operator=( elf_image&& ) = default;
+		elf_image& operator=( const elf_image& ) = default;
+
+		// Implement the interface requirements:
+		//
+		virtual size_t get_section_count() const override;
+		virtual section_descriptor get_section( size_t index ) const override;
+		virtual void modify_section( size_t index, const section_descriptor& desc ) override;
+		virtual uint64_t next_free_rva() const override;
+		virtual void add_section( section_descriptor& in_out, const void* data, size_t size ) override;
+		virtual void enum_relocations( const function_view<bool( const relocation_descriptor& )>& fn ) const override;
+		virtual void delete_relocation( const relocation_descriptor& desc ) override;
+		virtual uint64_t get_image_base() const override;
+		virtual size_t get_image_size() const override;
+		virtual bool has_relocations() const override;
+		virtual std::optional<uint64_t> get_entry_point() const override;
+		virtual size_t size() const override { return raw_bytes.size(); }
+		virtual void* data()  override { return raw_bytes.data(); }
+		virtual const void* cdata() const override { return raw_bytes.data(); }
+		virtual bool is_valid() const override;
+		virtual void flush() override;
+		virtual void add_relocation( uint64_t rva ) override { unreachable(); /* TODO */ }
+
+		// Helpers used to declare the functions.
+		//
+		bool is_elf64() const;
+		uint64_t get_alignment_mask() const;
+	};
+};
